9d9b52442159cee2d4dee87d78dd0fac5058ee3f
Mohsin-Khattak <96380711+Mohsin-Khattak@users.noreply.github.com>
2024-08-27 15:26:49 +0500

    async await add and also sqllite function move to db file

--- src/screens/home-screen/index.js
+++ src/screens/home-screen/index.js
@@ -16,8 +16,12 @@ import Lottie from 'lottie-react-native';
 import Medium from 'typography/medium-text';
 import HorizontalMarquee from 'components/molecules/marquee-txt/horizontal-marquee';
 import {convertToSixDigitNumber} from '../../utils/get-hashed-key';
-import RNFS from 'react-native-fs';
-import db from 'services/sqlite/db';
+
+import db, {
+  createTableIfNotExists,
+  downloadVideo,
+  saveDataToDatabase,
+} from 'services/sqlite/db';
 
 const Home = props => {
   const [playerId, setPlayerId] = React.useState('');
@@ -25,160 +29,81 @@ const Home = props => {
   const [playlist, setPlaylist] = React.useState({});
   const [nextIndex, setNextIndex] = React.useState(0);
   const [isConnected, setIsConnected] = React.useState(false);
+  // console.log('isConnected check===>', isConnected);
   const [loading, setLoading] = React.useState(true);
-
   console.log(width, height);
-  const createTableIfNotExists = () => {
-    db.transaction(tx => {
-      tx.executeSql(
-        `CREATE TABLE IF NOT EXISTS Playlist (
-          id INTEGER PRIMARY KEY AUTOINCREMENT,
-          video_time INTEGER,
-          video_title TEXT UNIQUE,
-          video_url TEXT,
-          widget_data TEXT,
-          localPath TEXT
-        )`,
-        [],
-        () => console.log('Table created or already exists'),
-        error => console.log('Error creating table', error),
-      );
-    });
-  };
 
   // Call this function when initializing the component
   const initializeDatabase = () => {
     createTableIfNotExists();
   };
 
-  const downloadVideo = async (url, fileName) => {
-    const path = `${RNFS.DocumentDirectoryPath}/${fileName}`;
-    const download = RNFS.downloadFile({
-      fromUrl: url,
-      toFile: path,
-    });
-    const result = await download.promise;
-    if (result.statusCode === 200) {
-      return path;
-    } else {
-      throw new Error('Failed to download video');
-    }
-  };
-
-  const saveDataToDatabase = async videos => {
-    if (!db) return;
-
-    db.transaction(tx => {
-      videos.forEach(video => {
-        // Check if video already exists
-        tx.executeSql(
-          'SELECT COUNT(*) AS count FROM Playlist WHERE video_title = ?',
-          [video.video_title],
-          (tx, results) => {
-            const count = results.rows.item(0).count;
-            if (count > 0) {
-              // If video exists, check if the URL is different
-              tx.executeSql(
-                'SELECT video_url FROM Playlist WHERE video_title = ?',
-                [video.video_title],
-                (tx, results) => {
-                  const existingVideoUrl = results.rows.item(0).video_url;
-                  if (existingVideoUrl !== video.video_url) {
-                    // Update existing video if the URL is different
-                    tx.executeSql(
-                      'UPDATE Playlist SET video_time = ?, video_url = ?, widget_data = ?, localPath = ? WHERE video_title = ?',
-                      [
-                        video.video_time,
-                        video.video_url,
-                        video.widget_data
-                          ? JSON.stringify(video.widget_data)
-                          : null,
-                        video.localPath,
-                        video.video_title,
-                      ],
-                      () => console.log('Video data updated successfully'),
-                      error => console.log('Error updating video data', error),
-                    );
-                  }
-                },
-                error => console.log('Error checking video URL', error),
-              );
-            } else {
-              // Insert new video if it doesn't exist
-              tx.executeSql(
-                'INSERT INTO Playlist (video_time, video_title, video_url, widget_data, localPath) VALUES (?, ?, ?, ?, ?)',
-                [
-                  video.video_time,
-                  video.video_title,
-                  video.video_url,
-                  video.widget_data ? JSON.stringify(video.widget_data) : null,
-                  video.localPath,
-                ],
-                () => console.log('Video data inserted successfully'),
-                error => console.log('Error inserting video data', error),
-              );
-            }
-          },
-          error => console.log('Error checking video existence', error),
-        );
-      });
-    });
-  };
-
   const getVideos = async () => {
     try {
-      if (!isConnected) {
-        const res = await getPlaylist(playerId);
-        const videoData = await Promise.all(
-          res?.data?.videos.map(async video => {
-            const localPath = await downloadVideo(
-              video.video_url,
-              `${video.video_title}.mp4`,
-            );
-            return {...video, localPath};
-          }),
-        );
-        setVideos(videoData);
-        setPlaylist(res?.data);
-        saveDataToDatabase(videoData);
-      }
+      const res = await getPlaylist(playerId);
+      const videoData = await Promise.all(
+        res?.data?.videos.map(async video => {
+          const localPath = await downloadVideo(
+            video.video_url,
+            `${video.video_title}.mp4`,
+          );
+          return {...video, localPath};
+        }),
+      );
+      setVideos(videoData);
+      setPlaylist(res?.data);
+      saveDataToDatabase(videoData);
     } catch (error) {
       console.log('Error fetching playlist', error);
     }
   };
 
-  const getLastSavedData = () => {
+  const getLastSavedData = async () => {
     if (!db) return;
 
-    db.transaction(tx => {
-      tx.executeSql(
+    const executeSqlAsync = (sql, params = []) => {
+      return new Promise((resolve, reject) => {
+        db.transaction(tx => {
+          tx.executeSql(
+            sql,
+            params,
+            (tx, results) => resolve(results),
+            (tx, error) => reject(error),
+          );
+        });
+      });
+    };
+
+    try {
+      const results = await executeSqlAsync(
         'SELECT video_time, video_title, video_url, widget_data, localPath FROM Playlist ORDER BY id DESC',
         [],
-        (tx, results) => {
-          if (results.rows.length > 0) {
-            const lastData = [];
-            for (let i = 0; i < results.rows.length; i++) {
-              const item = {
-                video_time: results.rows.item(i).video_time,
-                video_title: results.rows.item(i).video_title,
-                video_url: results.rows.item(i).video_url,
-                widget_data: results.rows.item(i).widget_data
-                  ? JSON.parse(results.rows.item(i).widget_data)
-                  : null,
-                localPath: results.rows.item(i).localPath,
-              };
-              lastData.push(item);
-            }
-            console.log('lastData check from sqlLite===>', lastData);
-            setVideos(lastData);
-            setPlaylist({videos: lastData});
-            console.log('last video check===>', playlist?.videos);
-          }
-        },
-        error => console.log('Error retrieving data', error),
       );
-    });
+
+      if (results.rows.length > 0) {
+        const lastData = [];
+        for (let i = 0; i < results.rows.length; i++) {
+          const item = {
+            video_time: results.rows.item(i).video_time,
+            video_title: results.rows.item(i).video_title,
+            video_url: results.rows.item(i).video_url,
+            widget_data: results.rows.item(i).widget_data
+              ? JSON.parse(results.rows.item(i).widget_data)
+              : null,
+            localPath: results.rows.item(i).localPath,
+          };
+          lastData.push(item);
+        }
+        console.log('lastData check from sqlLite===>', lastData);
+        setVideos(lastData);
+        setPlaylist({videos: lastData});
+        console.log('last video check===>', playlist?.videos);
+      }
+    } catch (error) {
+      console.log('Error retrieving data', error);
+    }
   };
+
   // Call initializeDatabase() inside your useEffect or initialization logic
   React.useEffect(() => {
     initializeDatabase();
@@ -224,34 +149,24 @@ const Home = props => {
     }
   }, [playerId]);
 
-  // React.useEffect(() => {
-  //   NetInfo.fetch().then(state => {
-  //     if (!state.isConnected) {
-  //       getLastSavedData();
-  //     }
-  //   });
-  // }, []);
   React.useEffect(() => {
     const unsubscribe = NetInfo.addEventListener(state => {
       console.log('Connection type', state.type);
       console.log('Is connected?', state.isConnected);
       if (!state?.isConnected) {
         getLastSavedData();
-      } else if (!playerId) {
-      } else {
-        getVideos();
       }
     });
     return () => unsubscribe();
   }, [playerId]);
 
-  // React.useEffect(() => {
-  //   if (!playerId) {
-  //     // Handle case where playerId does not exist
-  //   } else {
-  //     getVideos();
-  //   }
-  // }, [playerId]);
+  React.useEffect(() => {
+    if (!playerId) {
+      // Handle case where playerId does not exist
+    } else {
+      getVideos();
+    }
+  }, [playerId]);
 
   if (loading) {
     return <Loader />;
--- src/services/sqlite/db.js
+++ src/services/sqlite/db.js
@@ -1,6 +1,6 @@
 // db.js (or another suitable filename)
 import SQLite from 'react-native-sqlite-storage';
-
+import RNFS from 'react-native-fs';
 const db = SQLite.openDatabase(
   {name: 'mydatabase.db', location: 'default'},
   () => console.log('Database opened successfully'),
@@ -8,3 +8,110 @@ const db = SQLite.openDatabase(
 );
 
 export default db;
+
+export const createTableIfNotExists = async () => {
+  return new Promise((resolve, reject) => {
+    db.transaction(tx => {
+      tx.executeSql(
+        `CREATE TABLE IF NOT EXISTS Playlist (
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
+          video_time INTEGER,
+          video_title TEXT UNIQUE,
+          video_url TEXT,
+          widget_data TEXT,
+          localPath TEXT
+        )`,
+        [],
+        () => {
+          console.log('Table created or already exists');
+          resolve();
+        },
+        error => {
+          console.log('Error creating table', error);
+          reject(error);
+        },
+      );
+    });
+  });
+};
+
+export const saveDataToDatabase = async videos => {
+  if (!db) return;
+
+  const executeSqlAsync = (sql, params = []) => {
+    return new Promise((resolve, reject) => {
+      db.transaction(tx => {
+        tx.executeSql(
+          sql,
+          params,
+          (tx, results) => resolve(results),
+          (tx, error) => reject(error),
+        );
+      });
+    });
+  };
+
+  for (const video of videos) {
+    try {
+      // Check if video already exists
+      const results = await executeSqlAsync(
+        'SELECT COUNT(*) AS count FROM Playlist WHERE video_title = ?',
+        [video.video_title],
+      );
+      const count = results.rows.item(0).count;
+
+      if (count > 0) {
+        // If video exists, check if the URL is different
+        const urlResults = await executeSqlAsync(
+          'SELECT video_url FROM Playlist WHERE video_title = ?',
+          [video.video_title],
+        );
+        const existingVideoUrl = urlResults.rows.item(0).video_url;
+
+        if (existingVideoUrl !== video.video_url) {
+          // Update existing video if the URL is different
+          await executeSqlAsync(
+            'UPDATE Playlist SET video_time = ?, video_url = ?, widget_data = ?, localPath = ? WHERE video_title = ?',
+            [
+              video.video_time,
+              video.video_url,
+              video.widget_data ? JSON.stringify(video.widget_data) : null,
+              video.localPath,
+              video.video_title,
+            ],
+          );
+          console.log('Video data updated successfully');
+        }
+      } else {
+        // Insert new video if it doesn't exist
+        await executeSqlAsync(
+          'INSERT INTO Playlist (video_time, video_title, video_url, widget_data, localPath) VALUES (?, ?, ?, ?, ?)',
+          [
+            video.video_time,
+            video.video_title,
+            video.video_url,
+            video.widget_data ? JSON.stringify(video.widget_data) : null,
+            video.localPath,
+          ],
+        );
+        console.log('Video data inserted successfully');
+      }
+    } catch (error) {
+      console.log('Error saving video data', error);
+    }
+  }
+};
+
+export const downloadVideo = async (url, fileName) => {
+  const path = `${RNFS.DocumentDirectoryPath}/${fileName}`;
+  const download = RNFS.downloadFile({
+    fromUrl: url,
+    toFile: path,
+  });
+  const result = await download.promise;
+  if (result.statusCode === 200) {
+    return path;
+  } else {
+    throw new Error('Failed to download video');
+  }
+};
